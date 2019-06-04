359f05f24581b3fc7cbbb430b0e83acc5ac2277e
Ben Bhate <benbhate@gmail.com>
2019-06-04 18:29:35 +0530

    Get price api created

--- smilewidgets/products/admin.py
+++ smilewidgets/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, GiftCard, ProductPrice
+
+admin.site.register(Product)
+admin.site.register(GiftCard)
+admin.site.register(ProductPrice)
--- smilewidgets/smilewidgets/settings.py
+++ smilewidgets/smilewidgets/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
+
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
