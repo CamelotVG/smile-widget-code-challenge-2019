61c9b2449a51484687e45c744741fd7324984357
Egor Gorunovich <pixel.egor@gmail.com>
2019-09-18 13:07:14 +0300

    Add "get-price" api endpoint

--- smilewidgets/products/views.py
+++ smilewidgets/products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+@parser_classes([JSONParser])
+def get_price(request):
+    data = request.data
+    return Response(data)
